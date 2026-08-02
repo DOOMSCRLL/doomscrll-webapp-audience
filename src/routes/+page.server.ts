8a80e939bd7f4751f66aaa1de1ee43656c005fb4
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-02 21:27:15 +0300

    feat: Add load function to root route's server file, fetch project counts for today

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,12 @@
-
 import type { PageServerLoad } from "./$types"
+import { getProjectCountsFor } from "repos/project-repo"
+import DDate from "utils/d-date"
 
+export const load: PageServerLoad = async ({ fetch }) => {
+	const today = DDate.today()
+	const projectCounts = await getProjectCountsFor(today, fetch)
 
-export const load: PageServerLoad = async ({ fetch, url }) => {
-	const now = new Date()
+	return {
+		projectCounts,
+	}
 }
