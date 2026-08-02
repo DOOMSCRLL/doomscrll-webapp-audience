41beaf636c64ef1d22fa092adb0fd555ad7ed573
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-02 22:03:29 +0300

    fix: Update the load function for root route to handle error response gracefully

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,16 @@
-import type { PageServerLoad } from "./$types"
 import { getProjectCountsFor } from "repos/project-repo"
 import DDate from "utils/d-date"
+import type { PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const today = DDate.today()
-	const projectCounts = await getProjectCountsFor(today, fetch)
-
-	return {
-		projectCounts,
+	try {
+		const projectCounts = await getProjectCountsFor(today, fetch)
+		return {
+			projectCounts,
+		}
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		return { projectCounts: [] }
 	}
 }
