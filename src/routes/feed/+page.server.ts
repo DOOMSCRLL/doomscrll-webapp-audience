5020d9c784e60bb859d3cb37f909bf66d6c46225
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-04 00:56:26 +0300

    wip: Implement fetching for the feed route, and implement query support using URL search params, and add fallbacks for edge cases

--- src/routes/feed/+page.server.ts
+++ src/routes/feed/+page.server.ts
@@ -1,15 +1,36 @@
-import type { PageServerLoad } from "./$types"
-import { getProjectFeed } from "repos/project-repo"
+import { redirect } from "@sveltejs/kit"
 
-export const load: PageServerLoad = async ({ fetch, url }) => {
-	const category = url.searchParams.get("category") ?? undefined
+import type { PageServerLoad } from "./$types"
+
+import { getProjectFeed } from "repos/project-repo"
+import { DEFAULT_NUMBER_PAGE, DEFAULT_SIZE_BATCH_FEED, getFeedURLFor } from "utils/project-feed-utils"
+
+export const load: PageServerLoad = async ({ fetch, url, parent }) => {
+	const parentData = await parent()
+	if (parentData.projectCounts.length === 0) {
+		redirect(307, "/")
+	}
+
+	const categoryParam = url.searchParams.get("category")
+	const validCategory = parentData.projectCounts.find((item) => item.category === categoryParam)?.category
+	const category = validCategory ?? parentData.projectCounts[0].category
+
 	const tag = url.searchParams.get("tag") ?? undefined
 	const platform = url.searchParams.get("platform") ?? undefined
 	const pageParam = url.searchParams.get("page")
 	const batchSizeParam = url.searchParams.get("batchSize")
 
-	const page = pageParam ? Number(pageParam) : 1
-	const batchSize = batchSizeParam ? Number(batchSizeParam) : 16
+	const page = pageParam && !isNaN(Number(pageParam)) ? Number(pageParam) : DEFAULT_NUMBER_PAGE
+	const batchSize = batchSizeParam && !isNaN(Number(batchSizeParam)) ? Number(batchSizeParam) : DEFAULT_SIZE_BATCH_FEED
+
+	const targetUrl = getFeedURLFor(category, { tag, platform, page, batchSize })
+	if (
+		url.searchParams.get("category") !== category ||
+		url.searchParams.get("page") !== String(page) ||
+		url.searchParams.get("batchSize") !== String(batchSize)
+	) {
+		redirect(307, targetUrl)
+	}
 
 	const previews = await getProjectFeed(
 		{
