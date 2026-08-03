fa0715c7c22e4cc6d1f7bf6cb7b692f757da036c
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-04 00:53:22 +0300

    refactor: Move project counts fetching to the root layout, so it is available for both feed route and root route

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,20 @@
 import type { LayoutServerLoad } from "./$types"
 
+import type ProjectCategoryCounts from "models/internal/project-category-counts"
+import { getProjectCountsFor } from "repos/project-repo"
+import DDate from "utils/d-date"
+
 export const load: LayoutServerLoad = async ({ locals, fetch }) => {
+	let projectCounts: ProjectCategoryCounts[]
+	try {
+		projectCounts = await getProjectCountsFor(DDate.today(), fetch)
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		projectCounts = []
+	}
+
 	return {
 		locale: locals.locale,
+		projectCounts,
 	}
 }
