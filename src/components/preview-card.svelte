c5af1792a07b958503165c734cff7ad80cfe4cb9
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-04 00:50:34 +0300

    feat: Implement `PreviewCard` for project previews on the feed

---
+++ src/components/preview-card.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { ProjectPreview } from "models/project"
+	import SlabAnchor from "./buttons/slab-anchor.svelte"
+	import Icon from "./icons/icon.svelte"
+	import PreviewCoverImage from "./preview-cover-image.svelte"
+
+	type Props = {
+		preview: ProjectPreview
+	}
+
+	const { preview }: Props = $props()
+</script>
+
+<section class="flex flex-col items-center gap-4">
+	<p class="font-mono font-bold tracking-wider text-inverse">@{preview.authorUsername}</p>
+	<PreviewCoverImage src={preview.coverImagePath ?? ""} alt="MISSING_ALT" />
+	<section class="flex h-min w-full gap-4">
+		{#each preview.tags as tag (tag)}
+			<p class="font-serif font-bold whitespace-nowrap text-obverse group-even:italic">{tag}</p>
+		{/each}
+	</section>
+	<section class="grid w-full grid-cols-[auto_1fr_auto] items-center justify-items-center">
+		<Icon icon="Starmark" size="small" />
+		<p class="font-serif text-2xl font-medium text-inverse">{preview.name}</p>
+		<SlabAnchor
+			href="/project/{preview.referenceId}"
+			variant="text"
+			fit="square"
+			size="small"
+			ariaLabel="MISSING_LABEL">
+			<Icon icon="ArrowForward" size="small" />
+		</SlabAnchor>
+	</section>
+</section>
