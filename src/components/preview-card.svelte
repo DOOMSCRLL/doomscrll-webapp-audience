bd6cc5c026a635cb35bc99fc32a40e24fc9044fe
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-05 22:30:54 +0300

    refactor: Add missing aria labels and alt texts in the `PreviewCard`

--- src/components/preview-card.svelte
+++ src/components/preview-card.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { ProjectPreview } from "models/project"
 	import { generateColorFrom, hslStrToCss } from "utils/generate-color-from"
+
+	import { LocaleContext } from "contexts/shared.svelte"
+	import { getDictionaryOf } from "repos/locale-repo"
 	import SlabAnchor from "./buttons/slab-anchor.svelte"
 	import Icon from "./icons/icon.svelte"
 	import PreviewCoverImage from "./preview-cover-image.svelte"
@@ -10,11 +13,13 @@
 	}
 
 	const { preview }: Props = $props()
+
+	const dict = $derived(getDictionaryOf(LocaleContext.context.value!).feed.previewCard)
 </script>
 
 <section class="flex flex-col gap-4">
 	<p class="font-mono font-bold tracking-wider text-inverse">@{preview.authorUsername}</p>
-	<PreviewCoverImage src={preview.coverImagePath ?? ""} alt="MISSING_ALT" />
+	<PreviewCoverImage src={preview.coverImagePath ?? ""} alt="{preview.name} {dict.altCoverSuffix}" />
 	<section class="flex h-min w-full flex-wrap justify-center gap-4 gap-y-0">
 		{#each preview.tags as tag (tag)}
 			<p
@@ -35,7 +40,7 @@
 			variant="text"
 			fit="square"
 			size="small"
-			ariaLabel="MISSING_LABEL">
+			ariaLabel={dict.ariaPreviewAction}>
 			<Icon icon="ArrowForward" size="small" />
 		</SlabAnchor>
 	</section>
