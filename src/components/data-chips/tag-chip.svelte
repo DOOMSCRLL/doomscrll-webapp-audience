bf8c87434c04d0b185b52a504a6793443df468d3
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-06 22:13:12 +0300

    feat: Copy `TagChip` from DOOMLIT webapp repo; refactor: Add `isRemoveable` prop to `TagChip`, and change the removal button to render conditionally;

---
+++ src/components/data-chips/tag-chip.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type ProjectTag from "models/project-tag"
+	import { generateColorFrom, hslStrToCss } from "utils/generate-color-from"
+
+	import SlabButton from "comps/buttons/slab-button.svelte"
+	import Icon from "comps/icons/icon.svelte"
+	import { LocaleContext } from "contexts/shared.svelte"
+	import { getDictionaryOf } from "repos/locale-repo"
+
+	type Props = {
+		tag: ProjectTag
+		isRemoveable?: boolean
+		onRemove?: (tag: ProjectTag) => void
+	}
+
+	const { tag, isRemoveable = false, onRemove }: Props = $props()
+	const removeLabel = $derived(getDictionaryOf(LocaleContext.context.value!).common.dataChips.tag.ariaLabelRemove)
+	// TODO: Tweak coloring, size and generator.
+</script>
+
+<div class="group flex w-min items-center">
+	<div class="rounded-full px-4 py-2" style="background-color: {hslStrToCss(generateColorFrom(tag))}">
+		<p class="font-serif text-xl font-bold whitespace-nowrap text-obverse group-even:italic">
+			{tag}
+		</p>
+	</div>
+
+	{#if isRemoveable && onRemove}
+		<SlabButton ariaLabel={removeLabel} variant="text" hasAccent={true} fit="square" onClick={() => onRemove(tag)}>
+			<Icon icon="Cancel" />
+		</SlabButton>
+	{/if}
+</div>
