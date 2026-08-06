cb73b10554f6cb9ac650fc3c9bc7f8179f74e7ad
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-06 16:42:42 +0300

    refactor: Move visibility to use a bound flag `isOpen`, deprecate `trigger` and manual event injection to the `trigger`

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -1,54 +1,37 @@
 <script lang="ts">
 	import closedByAny from "attcs/closed-by-any"
 	import type { Snippet } from "svelte"
-	import SlabButton from "./buttons/slab-button.svelte"
 	import Icon from "./icons/icon.svelte"
 
 	type Props = {
 		header: string
-		closeLabel: string
 		children: Snippet
 		width?: "min" | "max"
-		trigger?: HTMLElement
+		isOpen?: boolean
 	}
 
-	const { header, closeLabel, width = "min", trigger = $bindable(), children }: Props = $props()
+	let { header, width = "min", isOpen = $bindable(false), children }: Props = $props()
 
-	let ref = $state<HTMLDialogElement>()
 	const id = `modal_${Math.random().toString(36).slice(2, 9)}`
 
+	let ref = $state<HTMLDialogElement>()
 	$effect(() => {
-		if (!trigger || !ref) return
-
-		const currentTrigger = trigger
-
-		currentTrigger.setAttribute("aria-haspopup", "dialog")
-		currentTrigger.setAttribute("aria-controls", id)
-		currentTrigger.setAttribute("aria-expanded", ref.open ? "true" : "false")
-
-		function openModal() {
-			ref?.showModal()
-		}
-
-		currentTrigger.addEventListener("click", openModal)
-		return () => {
-			currentTrigger.removeEventListener("click", openModal)
-			currentTrigger.removeAttribute("aria-haspopup")
-			currentTrigger.removeAttribute("aria-controls")
-			currentTrigger.removeAttribute("aria-expanded")
-		}
+		if (!ref) return
+		else if (isOpen && !ref.open) ref.showModal()
+		else if (!isOpen && ref.open) ref.close()
 	})
 
-	function closeModal() {
-		ref?.close()
+	function handleClose() {
+		isOpen = false
 	}
 </script>
 
 <dialog
-	{@attach closedByAny({ trigger })}
+	{@attach closedByAny({ trigger: undefined })}
 	{id}
 	aria-modal="true"
 	aria-labelledby="title_{id}"
+	onclose={handleClose}
 	class={[
 		width === "min" ? "w-[40vw]" : "w-full",
 		"m-auto h-min max-h-full p-4 open:z-50",
@@ -56,8 +39,6 @@
 		"flex-col items-start gap-4 open:box-border open:flex",
 	]}
 	bind:this={ref}>
-	<SlabButton alignment="center" fit="min" size="small" variant="text" renderDecors={true} onClick={closeModal}
-		>{closeLabel}</SlabButton>
 	<h2 id="title_{id}" class="inline-flex gap-2 font-serif text-4xl text-accent italic">
 		<Icon icon="Starmark" />
 		{header}
