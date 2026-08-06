6c15dbfa2243c8437bdcce1cc63cba7bb1a08530
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-06 22:17:10 +0300

    feat: Implement tag query feat to `QueryModal`; refactor: Add an `$effect` to sync selected data when modal is opened, to solve the issue of stale data; refactor: Invalidate `selectedTag` on category select flow; refactor: Update query submit flow to close the modal when it is done;

--- src/components/query-modal.svelte
+++ src/components/query-modal.svelte
@@ -4,37 +4,60 @@
 	import { LocaleContext } from "contexts/shared.svelte"
 	import type Category from "models/category"
 	import type ProjectCategoryCounts from "models/internal/project-category-counts"
+	import type ProjectTag from "models/project-tag"
 	import { getCategoryLabelFor, isCategory } from "repos/category-repo"
 	import { getDictionaryOf } from "repos/locale-repo"
 	import type { FeedQuery } from "repos/project-repo"
 
+	import { getTagsFor } from "repos/tag-repo"
 	import SlabButton from "./buttons/slab-button.svelte"
 	import DDropdown from "./d-dropdown/d-dropdown.svelte"
 	import Icon from "./icons/icon.svelte"
 	import Modal from "./modal.svelte"
+	import TagQueryInput from "./tag-query-input.svelte"
 
 	type Props = {
 		currentCategory: Category
 		availableCategories: ProjectCategoryCounts[]
+		currentTag?: ProjectTag
 		isOpen?: boolean
 		onQuerySubmit?: (query: FeedQuery) => void
 	}
 
-	let { currentCategory, availableCategories, isOpen = $bindable(false), onQuerySubmit }: Props = $props()
+	let { currentCategory, availableCategories, currentTag, isOpen = $bindable(false), onQuerySubmit }: Props = $props()
 
 	const dict = $derived(getDictionaryOf(LocaleContext.context.value!).feed.modalQuery)
 
 	let selectedCategory = $state<Category>(untrack(() => currentCategory))
-	let hasQueryChanged = $derived(selectedCategory !== currentCategory)
+	const availableTags = $derived<ProjectTag[]>(getTagsFor(selectedCategory))
+	let selectedTag = $state(untrack(() => currentTag))
+
+	$effect(() => {
+		if (isOpen) {
+			untrack(() => (selectedCategory = currentCategory))
+			untrack(() => (selectedTag = currentTag))
+		}
+	})
+
+	let hasQueryChanged = $derived(selectedCategory !== currentCategory || selectedTag !== currentTag)
+
 	function handleCategorySelect(category: string) {
-		if (isCategory(category) && category !== selectedCategory) selectedCategory = category
+		if (isCategory(category) && category !== selectedCategory) {
+			selectedCategory = category
+			selectedTag = undefined
+		}
 	}
 
 	function handleQuerySubmit() {
-		onQuerySubmit?.({
-			category: hasQueryChanged ? selectedCategory : undefined,
-		})
+		if (hasQueryChanged) {
+			onQuerySubmit?.({
+				category: selectedCategory,
+				tag: selectedTag,
+			})
+			handleClose()
+		}
 	}
+
 	function handleClose() {
 		isOpen = false
 	}
@@ -57,7 +80,9 @@
 		]}
 		selectedValue={selectedCategory}
 		onChange={handleCategorySelect} />
+	<hr class="w-full border border-inverse" />
 	<p class="font-serif text-2xl font-medium text-inverse">{dict.query.copy}</p>
+	<TagQueryInput {availableTags} bind:selectedTag />
 	<hr class="w-full border border-inverse" />
 	<SlabButton variant="filled" isDisabled={!hasQueryChanged} onClick={handleQuerySubmit}>
 		<Icon icon="Settings" />
