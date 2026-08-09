b95e00dee9dc5edfd0cde2e1f12648dfcc8dc5a2
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-09 17:30:32 +0300

    refactor/style: Implement a large screen layout, following a two layer card layout

--- src/routes/project/[referenceId]/+page.svelte
+++ src/routes/project/[referenceId]/+page.svelte
@@ -7,6 +7,7 @@
 	import FeatureChip from "comps/data-chips/feature-chip.svelte"
 	import PlatformChip from "comps/data-chips/platform-chip.svelte"
 	import TagChip from "comps/data-chips/tag-chip.svelte"
+	import DoomscrllWordmark from "comps/icons/doomscrll-wordmark.svelte"
 	import ScreenshotCarousel from "comps/screenshot-carousel.svelte"
 	import ScreenshotPreviewModal from "comps/screenshot-preview-modal.svelte"
 	import YoutubePlayer from "comps/youtube-player.svelte"
@@ -51,68 +52,82 @@
 	<meta name="description" content={dict.meta.description} />
 </svelte:head>
 
-<main class="flex h-screen w-full flex-col gap-6 overflow-y-auto pb-6 supports-[height:100dvh]:h-dvh">
-	<CoverImageHeader projectName={project.name} authorHandle={project.creator.username} src={project.coverImagePath!} />
-	<!-- Description -->
-	<p class="mx-6 font-serif text-2xl text-pretty text-inverse">{project.description}</p>
-	<!-- Trailer -->
-	{#if !project.videoUrl}
-		<section class="flex w-full flex-col gap-6">
-			<h2 class="ml-6 font-mono font-bold tracking-wide text-inverse uppercase underline">
-				{dict.details.trailer.label}
-			</h2>
-			<YoutubePlayer
-				url={project.videoUrl ?? "https://www.youtube.com/watch?v=VUwkKl7IL1w"}
-				projectName={project.name} />
+<main
+	class={[
+		"flex h-screen w-full flex-col gap-6 overflow-y-auto pb-6 supports-[height:100dvh]:h-dvh",
+		"md:grid md:grid-cols-[1fr_2fr] md:overflow-hidden md:p-0",
+	]}>
+	<div class="contents overflow-hidden md:flex md:h-full md:w-full md:flex-col md:justify-center md:gap-6">
+		<section aria-hidden={true} class="hidden md:flex md:h-12 md:w-full md:items-center md:justify-center">
+			<DoomscrllWordmark />
 		</section>
-	{/if}
-	<!-- Category -->
-	<section class="grid w-full grid-cols-[minmax(0,1fr)_minmax(0,2fr)] items-center px-6">
-		<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelCategory}</h2>
-		<p class="font-serif text-xl font-medium italic">{getCategoryLabelFor(project.category, locale)}</p>
-	</section>
-	<!-- Tags -->
-	<section class="grid w-full grid-cols-[minmax(0,1fr)_minmax(0,2fr)] px-6">
-		<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelTags}</h2>
-		<div class="flex flex-wrap gap-1">
-			{#each project.tags as tag (tag)}
-				<TagChip {tag} />
-			{/each}
-		</div>
-	</section>
-	<!-- Screenshots -->
-	<section class="flex w-full flex-col gap-6">
-		<h2 class="ml-6 font-mono font-bold tracking-wide text-inverse uppercase underline">
-			{dict.details.screenshots.label}
-		</h2>
-		<ScreenshotCarousel
-			srcs={/*project.screenshotPaths ?? */ testScreenshots}
+		<CoverImageHeader
 			projectName={project.name}
-			onPreviewClick={handleScreenshotExpand} />
-	</section>
-	<!-- Features -->
-	{#if project.features}
+			authorHandle={project.creator.username}
+			src={project.coverImagePath!} />
+	</div>
+	<div class="contents md:flex md:h-full md:w-full md:min-w-0 md:flex-col md:gap-6 md:overflow-y-auto md:py-6">
+		<!-- Description -->
+		<p class="mx-6 font-serif text-2xl text-pretty text-inverse">{project.description}</p>
+		<!-- Trailer -->
+		{#if !project.videoUrl}
+			<section class="flex w-full flex-col gap-6">
+				<h2 class="ml-6 font-mono font-bold tracking-wide text-inverse uppercase underline">
+					{dict.details.trailer.label}
+				</h2>
+				<YoutubePlayer
+					url={project.videoUrl ?? "https://www.youtube.com/watch?v=VUwkKl7IL1w"}
+					projectName={project.name} />
+			</section>
+		{/if}
+		<!-- Category -->
+		<section class="grid w-full grid-cols-[minmax(0,1fr)_minmax(0,2fr)] items-center px-6">
+			<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelCategory}</h2>
+			<p class="font-serif text-xl font-medium italic">{getCategoryLabelFor(project.category, locale)}</p>
+		</section>
+		<!-- Tags -->
 		<section class="grid w-full grid-cols-[minmax(0,1fr)_minmax(0,2fr)] px-6">
-			<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelFeats}</h2>
-			<div class="flex flex-wrap gap-2">
-				{#each project.features as feature (feature)}
-					<FeatureChip {feature} />
+			<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelTags}</h2>
+			<div class="flex flex-wrap gap-1">
+				{#each project.tags as tag (tag)}
+					<TagChip {tag} />
 				{/each}
 			</div>
 		</section>
-	{/if}
-	<!-- Platforms -->
-	<section class="flex w-full flex-col gap-6 px-6">
-		<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">
-			{dict.details.platforms.label}
-		</h2>
-		<PlatformChip href={project.primaryUrl} platform={project.primaryPlatform} />
-		{#if project.secondaryPlatforms}
-			{#each project.secondaryPlatforms as platform (platform.platform)}
-				<PlatformChip href={platform.url} platform={platform.platform} />
-			{/each}
+		<!-- Screenshots -->
+		<section class="flex w-full flex-col gap-6">
+			<h2 class="ml-6 font-mono font-bold tracking-wide text-inverse uppercase underline">
+				{dict.details.screenshots.label}
+			</h2>
+			<ScreenshotCarousel
+				srcs={/*project.screenshotPaths ?? */ testScreenshots}
+				projectName={project.name}
+				onPreviewClick={handleScreenshotExpand} />
+		</section>
+		<!-- Features -->
+		{#if project.features}
+			<section class="grid w-full grid-cols-[minmax(0,1fr)_minmax(0,2fr)] px-6">
+				<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">{dict.details.labelFeats}</h2>
+				<div class="flex flex-wrap gap-2">
+					{#each project.features as feature (feature)}
+						<FeatureChip {feature} />
+					{/each}
+				</div>
+			</section>
 		{/if}
-	</section>
+		<!-- Platforms -->
+		<section class="flex w-full flex-col gap-6 px-6">
+			<h2 class="font-mono font-bold tracking-wide text-inverse uppercase underline">
+				{dict.details.platforms.label}
+			</h2>
+			<PlatformChip href={project.primaryUrl} platform={project.primaryPlatform} />
+			{#if project.secondaryPlatforms}
+				{#each project.secondaryPlatforms as platform (platform.platform)}
+					<PlatformChip href={platform.url} platform={platform.platform} />
+				{/each}
+			{/if}
+		</section>
+	</div>
 </main>
 
 {#if expandedScreenshotUrl}
